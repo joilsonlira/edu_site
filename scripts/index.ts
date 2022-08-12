b5d52f3712c10627a78543cc48752e68bc1252d9
joilsonlira <joilson.lira@live.com>
2022-08-12 19:27:17 -0300

    UPDATE: deu ruim

--- scripts/index.ts
+++ scripts/index.ts
@@ -1,4 +1,5 @@
 const photo = document.querySelector('.photo_img');
+const menu_bar = <HTMLElement>document.getElementById('main_header');
 
 const array = ["./assets/images/assets_gallery/EG corpo todo tratada.jpeg",
 "./assets/images/assets_gallery/EG corpo todo.jpeg",
@@ -8,20 +9,16 @@ const array = ["./assets/images/assets_gallery/EG corpo todo tratada.jpeg",
 "./assets/images/assets_gallery/WhatsApp Image 2021-08-05 at 19.23.53.jpeg"]
 
 let i:number = 0
+
 setInterval(function(){
     photo?.classList.remove('teste')
     if(i > (array.length -1) ){
         i = 0
     }
-    
     document.querySelector('.photo')?.firstElementChild?.setAttribute('src', array[i])
     photo?.classList.toggle('teste')
     i = i + 1
-
 },15000)
-
-
-const menu_bar = <HTMLElement>document.getElementById('main_header');
 
 function active_scroll(){
     menu_bar.classList.toggle('ativo_scroll', scrollY > 0);

eac10364fb1e8fa455267b2c3155e3f257c7198a
joilsonlira <joilson.lira@live.com>
2022-08-17 18:41:09 -0300

    Mudanças a serem submetidas: 	deleted:    assets/images/EG corpo todo tratada.jpeg 	new file:   assets/images/Youtube-Thumbnail-Downloader.jpg 	renamed:    assets/images/EG no Forum Diversidade WTW19.jpeg -> assets/images/assets_gallery/EG no Forum Diversidade WTW19.jpeg 	renamed:    assets/images/Edu palestrando no WTW.png -> assets/images/assets_gallery_bg/Edu_palestrando_no_WTW.png 	new file:   assets/images/assets_gallery_bg/Foto2.png 	new file:   assets/images/assets_gallery_bg/Foto_3_WTW.jpeg 	new file:   assets/images/assets_gallery_bg/WhatsApp Image 2022-07-22 at 20.25 1.png 	deleted:    assets/images/fi_chevrons-right.svg 	new file:   business.html 	new file:   config_gallery.json 	modified:   index.html 	modified:   scripts/index.js 	modified:   scripts/index.ts 	new file:   styles/business.css 	modified:   styles/global.css 	modified:   styles/index.css

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,55 @@
+"use strict";
+var _a;
 var photo = document.querySelector('.photo_img');
+var bg_bussines = document.getElementById('bg_container');
+var menu_bar = document.getElementById('main_header');
 var array = ["./assets/images/assets_gallery/EG corpo todo tratada.jpeg",
     "./assets/images/assets_gallery/EG corpo todo.jpeg",
     "./assets/images/assets_gallery/WhatsApp Image 2021-08-05 at 19.23.48.jpeg",
     "./assets/images/assets_gallery/WhatsApp Image 2021-08-05 at 19.23.50.jpeg",
     "./assets/images/assets_gallery/WhatsApp Image 2021-08-05 at 19.23.51.jpeg",
     "./assets/images/assets_gallery/WhatsApp Image 2021-08-05 at 19.23.53.jpeg"];
+var array_bg = [
+    "./assets/images/assets_gallery_bg/Edu_palestrando_no_WTW.png",
+    "./assets/images/assets_gallery_bg/Foto2.png",
+    "./assets/images/assets_gallery_bg/Foto_3_WTW.jpeg"
+];
 var i = 0;
+console.log(array_bg.length);
+console.log((_a = bg_bussines === null || bg_bussines === void 0 ? void 0 : bg_bussines.firstElementChild) === null || _a === void 0 ? void 0 : _a.getAttribute("src"));
 setInterval(function () {
     var _a, _b;
-    photo === null || photo === void 0 ? void 0 : photo.classList.remove('teste');
-    if (i > (array.length - 1)) {
+    if (i > (array.length) || i < 0) {
         i = 0;
     }
+    else {
+        i = i + 1;
+    }
+    photo === null || photo === void 0 ? void 0 : photo.classList.remove('teste');
     (_b = (_a = document.querySelector('.photo')) === null || _a === void 0 ? void 0 : _a.firstElementChild) === null || _b === void 0 ? void 0 : _b.setAttribute('src', array[i]);
     photo === null || photo === void 0 ? void 0 : photo.classList.toggle('teste');
-    i = i + 1;
 }, 15000);
-var menu_bar = document.getElementById('main_header');
+setInterval(function () {
+    var _a, _b;
+    bg_bussines === null || bg_bussines === void 0 ? void 0 : bg_bussines.classList.remove('teste');
+    if (i >= (array_bg.length) || i < 0) {
+        i = 0;
+        (_a = bg_bussines === null || bg_bussines === void 0 ? void 0 : bg_bussines.firstElementChild) === null || _a === void 0 ? void 0 : _a.setAttribute("src", array_bg[i]);
+    }
+    else {
+        (_b = bg_bussines === null || bg_bussines === void 0 ? void 0 : bg_bussines.firstElementChild) === null || _b === void 0 ? void 0 : _b.setAttribute("src", array_bg[i]);
+        i = i + 1;
+    }
+    bg_bussines === null || bg_bussines === void 0 ? void 0 : bg_bussines.classList.toggle('teste');
+    console.log(i);
+}, 15000);
 function active_scroll() {
+    var _a;
+    var scr_logo = '#';
     menu_bar.classList.toggle('ativo_scroll', scrollY > 0);
+    console.log((_a = document.querySelector('.logo_img')) === null || _a === void 0 ? void 0 : _a.getAttribute("src"));
+    if (scrollY > 0) {
+        // logo.firstElementChild?.setAttribute('src', scr_logo)
+    }
 }
 window.addEventListener('scroll', active_scroll);
--- scripts/index.ts
+++ scripts/index.ts
@@ -1,5 +1,7 @@
 const photo = document.querySelector('.photo_img');
+const bg_bussines = <HTMLElement>document.getElementById('bg_container');
 const menu_bar = <HTMLElement>document.getElementById('main_header');
+
 
 const array = ["./assets/images/assets_gallery/EG corpo todo tratada.jpeg",
 "./assets/images/assets_gallery/EG corpo todo.jpeg",
@@ -8,20 +10,48 @@ const array = ["./assets/images/assets_gallery/EG corpo todo tratada.jpeg",
 "./assets/images/assets_gallery/WhatsApp Image 2021-08-05 at 19.23.51.jpeg",
 "./assets/images/assets_gallery/WhatsApp Image 2021-08-05 at 19.23.53.jpeg"]
 
+const array_bg = [
+    "./assets/images/assets_gallery_bg/Edu_palestrando_no_WTW.png",
+    "./assets/images/assets_gallery_bg/Foto2.png",
+    "./assets/images/assets_gallery_bg/Foto_3_WTW.jpeg"
+]
+
 let i:number = 0
+console.log(array_bg.length)
+console.log(bg_bussines?.firstElementChild?.getAttribute("src"))
+setInterval(function(){
+    if(i > (array.length) || i < 0){
+        i = 0
+    }else{
+        i = i + 1
+    }
+    photo?.classList.remove('teste')
+    document.querySelector('.photo')?.firstElementChild?.setAttribute('src', array[i])
+    
+    photo?.classList.toggle('teste')
+
+},15000)
 
 setInterval(function(){
-    photo?.classList.remove('teste')
-    if(i > (array.length -1) ){
+    bg_bussines?.classList.remove('teste')
+    if(i >= (array_bg.length) || i < 0){
         i = 0
+        bg_bussines?.firstElementChild?.setAttribute("src", array_bg[i])
+    }else{
+        bg_bussines?.firstElementChild?.setAttribute("src", array_bg[i])
+        i = i + 1
     }
-    document.querySelector('.photo')?.firstElementChild?.setAttribute('src', array[i])
-    photo?.classList.toggle('teste')
-    i = i + 1
+    bg_bussines?.classList.toggle('teste')
+    console.log(i)
 },15000)
 
 function active_scroll(){
+    let scr_logo:string = '#'
     menu_bar.classList.toggle('ativo_scroll', scrollY > 0);
+    console.log(document.querySelector('.logo_img')?.getAttribute("src"));
+    if (scrollY > 0){
+        // logo.firstElementChild?.setAttribute('src', scr_logo)
+    }
 }
 window.addEventListener('scroll', active_scroll);
 
